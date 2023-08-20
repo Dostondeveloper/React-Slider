de98273f27c6a5ea3fccf13c48804466e43735dd
Dostondeveloper <udostonbek53u@gmail.com>
2023-08-20 20:10:09 +0500

    Slider

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Review from './components/Review'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>hello</h2>
+      <Review />
+    </>
+  )
+}
+
+export default App
